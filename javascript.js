4fd85ce17876aeaba4b4d3c1d6c3d6baf65c8291
adf8888-cmyk <248556410+adf8888-cmyk@users.noreply.github.com>
2026-03-16 20:15:26 +0000

    Add controls and formatting to complete exercise
    - Buttons for grid size control, grid creation and reset
    - Basic CSS formatting
    - Randomise colours of each grid square
    - 10 level transparency to enable colour depth increae with each
    mouseover

--- javascript.js
+++ javascript.js
@@ -1,11 +1,38 @@
 // https://codesandbox.io/p/sandbox/flexbox-square-grid-f7oq2?file=%2Findex.html 
 // https://stackoverflow.com/questions/55224589/document-createelementdiv-with-a-class
 
+const clearButton = document.querySelector("#clear-button");
+const sketchContainer = document.querySelector("#sketch-container");
+const horizontalSizeInput = document.querySelector("#horizontalSizeInput");
+const horizontalSizeLabel = document.querySelector(".input-container-horizontal label");
+const verticalSizeInput = document.querySelector("#verticalSizeInput");
+const verticalSizeLabel = document.querySelector(".input-container-vertical label");
+let horizontalSize = 16;
+let verticalSize = 16;
+
+clearButton.addEventListener("click", () => {
+    deleteSketchGrid();
+    createSketchGrid(verticalSize,horizontalSize);
+});
+
+horizontalSizeInput.addEventListener("input", (e) => {
+    horizontalSize = e.target.value;
+    horizontalSizeLabel.textContent = `${horizontalSize} across`;
+});
+
+verticalSizeInput.addEventListener("input", (e) => {
+    verticalSize = e.target.value;
+    verticalSizeLabel.textContent = `${verticalSize} down`;
+});
+
+
+
+function deleteSketchGrid(){
+    sketchContainer.innerHTML = '';
+  }
+
+
 function createSketchGrid(horizontal, vertical){
-    let sketchContainer = document.querySelector('#sketch-container');
-
-
-    
 
     for (let h = 0; h < horizontal; h++) {
     
@@ -20,23 +47,32 @@ function createSketchGrid(horizontal, vertical){
             let newSpan = document.createElement("span");
             sketchGridBox.appendChild(newSpan);
             sketchGridBox.className = "gridBox";
+            sketchGridBox.style.backgroundColor = "rgb(255, 255, 255)";
+            sketchGridBox.style.opacity = "1";
+            sketchGridBox.InitialBackgroundColor = `rgb(${generateRandomColor()}, ${generateRandomColor()}, ${generateRandomColor()})`;
+            sketchGridBox.InitialBackgroundOpacity = 0.1;
             sketchGridBox.addEventListener("mouseenter", () => {
-                sketchGridBox.style.background = "blue";
+ //               let bgColor = "rgb(255, 255, 255)";
+ //               let opacity = 1.0;
+                sketchGridBox.style.backgroundColor = sketchGridBox.InitialBackgroundColor;
+                sketchGridBox.style.opacity = sketchGridBox.InitialBackgroundOpacity;
+                sketchGridBox.InitialBackgroundOpacity = sketchGridBox.InitialBackgroundOpacity + 0.1;
+                
+
             });
 
-    //        newSpan.textContent = ("H" + h + "V" + v);
 
             gridRowDiv.appendChild(sketchGridBox);
         }
     }
 
-
-
-
-
-
 }   
 
+function generateRandomColor() {
+    return Math.floor(Math.random() * 255 + 1);
+  }
 
-createSketchGrid(64,64);
+
+
+  createSketchGrid(verticalSize,horizontalSize);
 
